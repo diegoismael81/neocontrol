825a9f4d860f722e807b753fc0b65b05d106da27
diegoismael <diego.ismael.81@gmail.com>
2020-11-19 22:45:45 -0500

    Send Email from GitHub

--- js/main.js
+++ js/main.js
@@ -3,16 +3,40 @@ $(function () {
 
 
 	/*--------------------------
-	sendForm
+	sendQuote
 	---------------------------- */
-	var form = $('#form-quote'); // contact form	
+	var quote = $('#form-quote'); // contact form	
 
 	// form submit event
-	form.on('submit', function (e) {
+	quote.on('submit', function (e) {
 		e.preventDefault(); // prevent default form submit
 
 		$.ajax({
-			url: 'https://southamerica-east1-emailsender-295900.cloudfunctions.net/neocontrol/sendEmail', // form action url						
+			url: 'https://southamerica-east1-emailsender-295900.cloudfunctions.net/neocontrol/sendQuote', // form action url						
+			type: 'POST', // form submit method get/post			
+			dataType: 'json', // request type html/json/xml
+			data: quote.serialize(), // serialize form data			
+			success: function (data) {								
+				location.reload(); // reset page
+			},
+			error: function (e) {
+				console.log(e)
+			}
+		});
+	});
+
+
+	/*--------------------------
+	sendQuote
+	---------------------------- */
+	var contact = $('#form-contact'); // contact form	
+
+	// form submit event
+	contact.on('submit', function (e) {
+		e.preventDefault(); // prevent default form submit
+
+		$.ajax({
+			url: 'https://southamerica-east1-emailsender-295900.cloudfunctions.net/neocontrol/sendContact', // form action url						
 			type: 'POST', // form submit method get/post			
 			dataType: 'json', // request type html/json/xml
 			data: form.serialize(), // serialize form data			
@@ -24,8 +48,6 @@ $(function () {
 			}
 		});
 	});
-
-
 
 });
 
