78d10eab74e997e2ff45037de3cb66e6b99d1ab1
diegoismael <diego.ismael.81@gmail.com>
2020-11-19 22:23:07 -0500

    Send Email from Local

--- js/main.js
+++ js/main.js
@@ -12,14 +12,12 @@ $(function () {
 		e.preventDefault(); // prevent default form submit
 
 		$.ajax({
-			url: 'https://southamerica-east1-emailsender-295900.cloudfunctions.net/email-neo-control', // form action url			
-			headers: {  'Access-Control-Allow-Origin': '*', 'Content-Type' : 'application/json' },
+			url: 'https://southamerica-east1-emailsender-295900.cloudfunctions.net/neocontrol/sendEmail', // form action url						
 			type: 'POST', // form submit method get/post			
 			dataType: 'json', // request type html/json/xml
 			data: form.serialize(), // serialize form data			
-			success: function (data) {				
-				console.log(data);
-				form.trigger('reset'); // reset form				
+			success: function (data) {								
+				location.reload(); // reset page
 			},
 			error: function (e) {
 				console.log(e)
